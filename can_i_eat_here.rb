4e31bc7adfcd85328cb1632a0c77a16efae9edb9
Zee Spencer <zee@zeespencer.com>
2014-07-19 22:03:45 -0700

    Allow Users to log in
    Well well well, we're finally getting somewhere interesting!
    
    We've done two things:
    
    1. We've written a log-in method
    2. We've started to use sessions to persist data across a single users
       many requests.
    
    On Sessions
    ===========
    
    Here we've got the notion of "sessions". A session uniquely identify a
    given visitor of your website. It lets you persist data over many HTTP
    requests. Ever wonder how Google or Facebook or Amazon knows you're you?
    
    Well, they use a session.
    
    We're using a session to store the ID of the user who logged
    in; and when we see that we have that information we don't ask people to
    sign up or log in on the homepage. Later we'll use it for more fancy
    stuff.
    
    The cool thing about sessions is they're *very difficult* to tamper
    with. All the data in the session is transmitted to the server and back
    to the client on every HTTP request; but it's encrypted!
    
    On Login Procedures
    ===================
    The other bit to keep in mind is the procedure we're using for verifying
    log-ins. Because we're storing the password in the database as a string,
    we can't just query the email and password at the same time. Instead we
    have to retrieve based on the email address, then compare the password
    stored in the database to the password the user typed in.
    
    If all that goes as planned, we stick their user id in the session! BAM!
    Now we know who that person is even if they close the web page. They
    would have to clear their cookies to stop us from knowing who they are.

--- can_i_eat_here.rb
+++ can_i_eat_here.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
 require_relative 'models'
 
+enable :sessions
+# Docs: http://www.sinatrarb.com/faq.html#sessions
+
+
 get "/" do
   erb :home
 end
@@ -16,5 +20,36 @@ post "/users" do
     redirect "/"
   else
     erb :new_user
+  end
+end
+
+get "/sessions/new" do
+  @login_attempt = User.new
+  erb :new_session
+end
+
+post "/sessions" do
+  # First, we try and find a user who actually matches that email address
+  @login_attempt = User.first({ :email => params[:user]["email"] })
+
+  # Then, we check if that users password matches the inputted password
+  if @login_attempt && @login_attempt.password == params[:user]["password"]
+
+    # If it does, we store the users ID in the `session` hash
+    session[:current_user] = @login_attempt.id
+
+    # And we send them back to the home page, since they're all logged in!
+    redirect "/"
+  else
+    # If we didn't find a user, or the password was wrong...
+
+    # "Instantiate" a user that isn't in the database
+    @login_attempt = User.new
+
+    # Stick some errors in it so we can let the person know what was up.
+    @login_attempt.errors.add(:password, "Couldn't find a user with that email/password combination")
+
+    # Then re-render that view!
+    erb :new_session
   end
 end
