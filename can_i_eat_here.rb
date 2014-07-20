184cd2539c03bc88f94b04d7fb6251488d0a080f
Zee Spencer <zee@zeespencer.com>
2014-07-20 12:10:30 -0700

    Registering logs you in
    Now we introduce the concept of sinatra helpers. Sinatra Helpers allow
    you to define methods that:
    
    1. Have access to `instance variables` defined in your routes
    2. May be called in your views
    
    Here I'm using helpers to wrap access to the `session` hash. This allows
    me to:
    
    * Load the current_user from the database and cache it once per request;
      even if I access to it several different times.
    * `login` a user, I.e. set the session variable and redirect.
    * Provide a `logged_in?` helper to reveal our intent better instead of
      asking `if session[:current_user]`
    
    By doing this, I can encourage restricting access to the `session` hash;
    which frequently becomes a den of complexity in web applications.

--- can_i_eat_here.rb
+++ can_i_eat_here.rb
@@ -3,6 +3,22 @@ require_relative 'models'
 
 enable :sessions
 
+helpers do
+  def current_user
+    @current_user ||= User.find(session[:current_user])
+  end
+
+  def login(user)
+    @current_user = user
+    session[:current_user] = user.id
+    redirect "/"
+  end
+
+  def logged_in?
+    !session[:current_user].nil?
+  end
+end
+
 get "/" do
   erb :home
 end
@@ -15,7 +31,7 @@ end
 post "/users" do
   @user = User.create(params[:user])
   if @user.saved?
-    redirect "/"
+    login(@user)
   else
     erb :new_user
   end
@@ -30,8 +46,7 @@ post "/sessions" do
   @login_attempt = User.first({ :email => params[:user]["email"] })
 
   if @login_attempt && @login_attempt.password == params[:user]["password"]
-    session[:current_user] = @login_attempt.id
-    redirect "/"
+    login(@login_attempt)
   else
     @login_attempt = User.new
     @login_attempt.errors.add(:password, "Couldn't find a user with that email/password combination")
