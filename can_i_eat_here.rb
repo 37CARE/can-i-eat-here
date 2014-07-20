2a0c5aef2394f1201c9db57921dabd6e51876ed1
Zee Spencer <zee@zeespencer.com>
2014-07-19 22:15:30 -0700

    Remove comments from previous commit

--- can_i_eat_here.rb
+++ can_i_eat_here.rb
@@ -2,8 +2,6 @@ require 'sinatra'
 require_relative 'models'
 
 enable :sessions
-# Docs: http://www.sinatrarb.com/faq.html#sessions
-
 
 get "/" do
   erb :home
@@ -29,27 +27,14 @@ get "/sessions/new" do
 end
 
 post "/sessions" do
-  # First, we try and find a user who actually matches that email address
   @login_attempt = User.first({ :email => params[:user]["email"] })
 
-  # Then, we check if that users password matches the inputted password
   if @login_attempt && @login_attempt.password == params[:user]["password"]
-
-    # If it does, we store the users ID in the `session` hash
     session[:current_user] = @login_attempt.id
-
-    # And we send them back to the home page, since they're all logged in!
     redirect "/"
   else
-    # If we didn't find a user, or the password was wrong...
-
-    # "Instantiate" a user that isn't in the database
     @login_attempt = User.new
-
-    # Stick some errors in it so we can let the person know what was up.
     @login_attempt.errors.add(:password, "Couldn't find a user with that email/password combination")
-
-    # Then re-render that view!
     erb :new_session
   end
 end
